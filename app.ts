7a08e0d347b29a07d569296411a444341df81615
pynghk01 <pyng@hk01.com>
2023-10-16 19:32:14 +0800

    fix: testing bug fix on standard version

--- app.ts
+++ app.ts
@@ -3,3 +3,5 @@ console.log("hello world!");
 console.log("testing CHANGELOG when skip github tags v1.2.0");
 
 console.log("testing CHANGELOG feature update minor version");
+
+console.log("testing bug fix");
